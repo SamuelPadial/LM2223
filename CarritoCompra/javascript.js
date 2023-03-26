fd43be693041c31daa376fb514edaab872a60c59
SamuelPadial <114154158+SamuelPadial@users.noreply.github.com>
2023-03-27 00:42:45 +0200

    Add files via upload

---
+++ CarritoCompra/javascript.js
@@ -0,0 +1,14 @@
+let objetosarray = [
+    {id:0,nombre:'Logitech K120 Teclado',precio:9.90,uds:0},
+    {id:1,nombre:'Monitor BenQ EW3270U 31.5" LED UltraHD 4K',precio:329,uds:0},
+    {id:2,nombre:'Logitech B100 Ratón Negro',precio:8.39,uds:0},
+    {id:3,nombre:'ASUS ExpertCenter D5 Tower',precio:520, uds:0}
+];
+
+
+function anadir(producto){
+    objetosarray[id] = producto;
+    for(i=0;i<4;i++){
+
+    }
+}
