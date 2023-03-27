621f1a41b185ab031255d63d85cd7a9aaa59059d
SamuelPadial <114154158+SamuelPadial@users.noreply.github.com>
2023-03-27 12:26:11 +0200

    Add files via upload

--- CarritoCompra/javascript.js
+++ CarritoCompra/javascript.js
@@ -5,10 +5,51 @@ let objetosarray = [
     {id:3,nombre:'ASUS ExpertCenter D5 Tower',precio:520, uds:0}
 ];
 
+let almacenado=null;
+
+function iniciar(){
+    almacenado=localStorage.getItem("producto");
+    if(alamcenado!=null){
+        almacenado = JSON.parse(almacenado);
+        producto = almacenado;
+        refrescar();
+    }
+}
 
 function anadir(producto){
-    objetosarray[id] = producto;
-    for(i=0;i<4;i++){
+    let objeto = objetosarray[producto];
+    objeto[uds]+=1;
+    objetosarray[producto] = objeto[uds];
+    document.getElementById("id"+producto).value = objeto[uds];
+    localStorage.setItem("producto",JSON.stringify(objetosarray));
+    refrescar();
 
+};
+
+function quitar(producto){
+    let objeto = objetosarray[producto];
+    let numuds = objeto[uds];
+    if(numuds>=1){
+        objeto[uds] -= 1;
+        objetosarray[producto] = objeto;
+        document.getElementById("id"+producto).value = objeto[uds];
+        localStorage.setItem("producto", JSON.stringify(objetosarray));
+        refrescar();
+    };
+}
+
+function refrescar(){
+    let listaprod = document.getElementById("carrito");
+    let total = 0;
+    listaprod.innerHTML="";
+    for (var i=0;i<objetosarray.length;i++){
+        let producto = objetosarray[i];
+        if(producto[uds]>0){
+            let nodo = document.createElement("li");
+            let texto  = document.createTextNode("Producto: " +producto[nombre]+
+            document.getElementById("id"+i).value = producto[uds];
+            nodo.appendChild(nodo);
+            total = total + producto[uds]*producto[precio];
+        }
     }
 }
